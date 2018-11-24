416dfcdd418483e54ac744092af81ff5223d6e63
Alejandro Velásquez Molina <alejandrovelasquezmolina@gmail.com>
2018-11-23 19:17:50 -0500

    Config and initial views

---
+++ src/routes/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/', (req,res) => {
+  res.send('index');
+});
+
+router.get('/about', (req, res) => {
+  res.send('about')
+});
+
+module.exports = router ;
