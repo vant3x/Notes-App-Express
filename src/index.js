cac38f92f5948eb3a1b71f6905bd05e42e111d91
Alejandro Velásquez Molina <alejandrovelasquezmolina@gmail.com>
2018-11-26 04:52:53 -0500

    Template Engine

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
 app.engine('.hbs', exphbs({
   defaultLayout: 'main',
-  layoutDir: path.join(app.get('views'),'layouts'),
+  layoutsDir: path.join(app.get('views'),'layouts'),
   partialsDir: path.join(app.get('views'), 'partials'),
   extname: '.hbs'
 }));
@@ -32,8 +32,8 @@ app.use(session({
 
 // Routes
 app.use(require('./routes/index'));
-app.use(require('./routes/users'));
 app.use(require('./routes/notes'));
+app.use(require('./routes/users'));
 
 // Static Files
 app.use(express.static(path.join(__dirname, 'public')));
